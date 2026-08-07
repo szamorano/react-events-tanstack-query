74d1d72109d62d4362df4bfec412edae4cc21205
Steven Zamorano <stevenzamorano.code@gmail.com>
2026-08-07 13:56:16 -0400

    Add queryKey, pull out elements needed,  and improve error message.

--- src/components/Events/NewEventsSection.jsx
+++ src/components/Events/NewEventsSection.jsx
@@ -5,19 +5,23 @@ import { useQuery } from "@tanstack/react-query";
 import { fetchEvents } from "../../util/http.js";
 
 export default function NewEventsSection() {
-  useQuery({
+  const { data, isPending, isError, error } = useQuery({
+    queryKey: ["events"],
     queryFn: fetchEvents,
   });
 
   let content;
 
-  if (isLoading) {
+  if (isPending) {
     content = <LoadingIndicator />;
   }
 
-  if (error) {
+  if (isError) {
     content = (
-      <ErrorBlock title="An error occurred" message="Failed to fetch events" />
+      <ErrorBlock
+        title="An error occurred"
+        message={error.info?.message || "Failed to fetch events."}
+      />
     );
   }
 
