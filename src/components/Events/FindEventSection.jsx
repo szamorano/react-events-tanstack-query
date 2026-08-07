c2de09e1ef71fcb4cf2cd9a2a8ed002a674cdcaa
Steven Zamorano <stevenzamorano.code@gmail.com>
2026-08-07 15:31:27 -0400

    Use pulled elements from useQuery in the component output.

--- src/components/Events/FindEventSection.jsx
+++ src/components/Events/FindEventSection.jsx
@@ -1,6 +1,9 @@
 import { useQuery } from "@tanstack/react-query";
 import { useRef, useState } from "react";
 import { fetchEvents } from "../../util/http";
+import LoadingIndicator from "../UI/LoadingIndicator";
+import ErrorBlock from "../UI/ErrorBlock";
+import EventItem from "./EventItem";
 
 export default function FindEventSection() {
   const searchElement = useRef();
@@ -16,6 +19,33 @@ export default function FindEventSection() {
     setSearchTerm(searchElement.current.value);
   }
 
+  let content = <p>Please enter a search term and to find events.</p>;
+
+  if (isPending) {
+    content = <LoadingIndicator />;
+  }
+
+  if (isError) {
+    content = (
+      <ErrorBlock
+        title="An error occurred"
+        message={error.info?.message || "Failed to fetch events."}
+      />
+    );
+  }
+
+  if (data) {
+    content = (
+      <ul className="events-list">
+        {data.map((event) => (
+          <li key={event.id}>
+            <EventItem event={event} />
+          </li>
+        ))}
+      </ul>
+    );
+  }
+
   return (
     <section className="content-section" id="all-events-section">
       <header>
@@ -29,7 +59,7 @@ export default function FindEventSection() {
           <button>Search</button>
         </form>
       </header>
-      <p>Please enter a search term and to find events.</p>
+      {content}
     </section>
   );
 }
