b1e21a4ec803a444e2c413461c3c830a1eec1ac7
Steven Zamorano <stevenzamorano.code@gmail.com>
2026-08-07 14:31:33 -0400

    Cache and stale data.

--- src/components/Events/NewEventsSection.jsx
+++ src/components/Events/NewEventsSection.jsx
@@ -8,6 +8,8 @@ export default function NewEventsSection() {
   const { data, isPending, isError, error } = useQuery({
     queryKey: ["events"],
     queryFn: fetchEvents,
+    staleTime: 5000,
+    // gcTime: 1000
   });
 
   let content;
