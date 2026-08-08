1aa0a306ea4fcd37e6387979b3dcf37f09b3ffb7
Steven Zamorano <stevenzamorano.code@gmail.com>
2026-08-08 12:24:21 -0400

    Format the displayed date.

--- src/components/Events/EventDetails.jsx
+++ src/components/Events/EventDetails.jsx
@@ -37,6 +37,11 @@ export default function EventDetails() {
   }
 
   if (data) {
+    const formattedDate = new Date(data.date).toLocaleDateString("en-US", {
+      day: "numeric",
+      month: "short",
+      year: "numeric",
+    });
     content = (
       <>
         <header>
@@ -52,7 +57,7 @@ export default function EventDetails() {
             <div>
               <p id="event-details-location">{data.location}</p>
               <time dateTime={`Todo-DateT$Todo-Time`}>
-                {data.date} @ {data.time}
+                {formattedDate} @ {data.time}
               </time>
             </div>
             <p id="event-details-description">{data.description}</p>
